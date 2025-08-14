456e060fce160128d7ec892aad6bcca04d073c07
suman samantray <10929249+suman-samantray@users.noreply.github.com>
2025-08-14 07:21:16 -0700

    Delete pympsi-PCA directory

--- pympsi-PCA/run_in_gppenv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# run_in_gppenv.sh — wrapper to activate gppenv and then exec any Python script
-
-source "/ccs/home/sama578/miniconda3/etc/profile.d/conda.sh"
-conda activate gppenv
-exec python "$@"
-
